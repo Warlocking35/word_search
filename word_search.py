b3d82414221f0955c4400ab01dba8157eaa4cbd7
Drayton Larimore-Rowe <122067963+Warlocking35@users.noreply.github.com>
2024-07-31 16:23:10 -0400

    Add files via upload

---
+++ word_search.py
@@ -0,0 +1,36 @@
+def word_search():
+    """
+    The user can input any phrase or passage, and also the word
+    that will be searched for.
+
+    :return: The word and how many were found.
+    """
+
+    connected_word = []  # Empty list that the matching words fall into
+    number_cw = 0  # The amount of times a word is found
+
+    while True:  # ends if the input is 0
+        print("Text to search (Press 0 to exit): ")
+        phrase_input = str(input("-> ")).casefold()
+        if phrase_input == '0':  # Checks to see if the input is 0
+            break
+
+        phrase_split = phrase_input.split()  # Creates a new list
+
+        print("Word to search for (Press 0 to exit): ")
+        word_selection = str(input("-> ")).casefold()
+        if word_selection == '0':  # Checks to see if the input is 0
+            break
+
+        for words in phrase_split:
+            if word_selection in words:
+                connected_word.append(words)
+                number_cw += 1
+
+        print("{}: found {} times"
+              .format(word_selection, number_cw))
+        number_cw = 0
+    print("Come again!")
+
+
+word_search()
